bec4f36903d979d5a27f11007b1f1a872a4f3157
nando-castro <fernando.castro10.master@gmail.com>
2024-11-27 23:32:16 -0300

    update Scanner.java alteração de métodos para novas categorias

--- src/main/java/br/ufma/ecp/token/TokenType.java
+++ src/main/java/br/ufma/ecp/token/TokenType.java
@@ -1,10 +1,20 @@
 package br.ufma.ecp.token;
 
 import java.util.List;
-import java.util.Map;
 
 public enum TokenType {
-    PLUS,MINUS,
+     // symbols
+     LPAREN,RPAREN,
+     LBRACE, RBRACE,
+     LBRACKET,RBRACKET,
+     
+     COMMA, SEMICOLON, DOT,
+       
+     PLUS,  MINUS,ASTERISK, SLASH,
+     
+     AND, OR, NOT,
+     
+     LT, GT, EQ,
 
      // Literals.
      NUMBER,
@@ -15,11 +25,10 @@ public enum TokenType {
 
  
      // keywords
-     METHOD,
-     WHILE,
-     IF,
-     CLASS,
-     CONSTRUCTOR,
+     WHILE, CLASS,CONSTRUCTOR,FUNCTION,
+     METHOD,FIELD,STATIC,VAR,INT,
+     CHAR,BOOLEAN,VOID,TRUE,FALSE,
+     NULL,THIS,LET,DO,IF,ELSE, RETURN,
 
      EOF,
 
@@ -34,11 +43,10 @@ public enum TokenType {
     static public boolean isKeyword (TokenType type) {
         List<TokenType> keywords  = 
             List.of(
-                METHOD,
-                WHILE,
-                IF,
-                CLASS,
-                CONSTRUCTOR
+                WHILE, CLASS,CONSTRUCTOR,FUNCTION,
+                METHOD,FIELD,STATIC,VAR,INT,
+                CHAR,BOOLEAN,VOID,TRUE,FALSE,
+                NULL,THIS,LET,DO,IF,ELSE, RETURN
             );
             return keywords.contains(type);
     }
