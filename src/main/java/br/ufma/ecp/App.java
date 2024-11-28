2f23721b1c7512652c347ed872933b42e28d8eef
nando-castro <fernando.castro10.master@gmail.com>
2024-11-27 23:34:10 -0300

    Adição de comentários

--- src/main/java/br/ufma/ecp/App.java
+++ src/main/java/br/ufma/ecp/App.java
@@ -1,8 +1,6 @@
 package br.ufma.ecp;
 
-import static br.ufma.ecp.token.TokenType.*;
-
-
+import static br.ufma.ecp.token.TokenType.EOF;
 
 import br.ufma.ecp.token.Token; 
 
@@ -14,11 +12,23 @@ public class App
     {
 
     
-        String input = "45  + if - \"ola\" laranja 876";
+        String input = """
+      // é um comentario 10
+      45 \"hello\" variavel + while < , if
+      /*
+      comentario em bloco
+      */
+      42 ola
+      
+      """;
         Scanner scan = new Scanner (input.getBytes());
         for (Token tk = scan.nextToken(); tk.type != EOF; tk = scan.nextToken()) {
             System.out.println(tk);
         }
+
+        // ALUNOS
+        // FILIPI PINHEIRO
+        // FERNANDO CASTRO
 
         /*
         Parser p = new Parser (input.getBytes());
