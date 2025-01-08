53c3b7adc17d2c8759e76c5f697e372cc3cf82bc
Filipe Pinheiro <76874327+filipe-pinheiro@users.noreply.github.com>
2025-01-07 21:36:08 -0300

    methodsConstructorTest
    FInalização do teste methodsConstructorTest

--- src/main/java/br/ufma/ecp/Parser.java
+++ src/main/java/br/ufma/ecp/Parser.java
@@ -594,14 +594,26 @@ public class Parser {
         while (peekTokenIs(VAR)) {
             parseVarDec();
         }
-				var nlocals = symTable.varCount(Kind.VAR);
+        var nlocals = symTable.varCount(Kind.VAR);
 
         vmWriter.writeFunction(functionName, nlocals);
+
+        if (subroutineType == CONSTRUCTOR) {
+            vmWriter.writePush(Segment.CONST, symTable.varCount(Kind.FIELD));
+            vmWriter.writeCall("Memory.alloc", 1);
+            vmWriter.writePop(Segment.POINTER, 0);
+        }
+
+        if (subroutineType == METHOD) {
+            vmWriter.writePush(Segment.ARG, 0);
+            vmWriter.writePop(Segment.POINTER, 0);
+        }
 
         parseStatements();
         expectPeek(RBRACE);
         printNonTerminal("/subroutineBody");
     }
+    
 
     private Segment kind2Segment(Kind kind) {
         if (kind == Kind.STATIC)
